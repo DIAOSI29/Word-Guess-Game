dc657e7c7e7d5ce3177397c06554179daa70862a
TianQin <qintian29@gmail.com>
2019-10-02 22:07:33 +0800

    welcome page layout and content set up

--- game.js
+++ game.js
@@ -1,34 +1,75 @@
-// example of onkeyup function from last assignment:
-// document.onkeyup = function() {
-//     var key = event.key;
-//     if (key === "a") {
-//       car.driveToWork();
-//     } else if (key === "b") {
-//       car.driveAroundWorld();
-//     } else if (key === "c") {
-//       car.getTuneUp();
-//     } else if (key === "d") {
-//       car.honk();
-//     }
-//   };
-
 //set up initial state of the game//
-var isGameOn = "false";
+var isGameOn = false;
+//set up info panel text//
+var resultText = "Guess Which Animal Is Behind The Scene ?";
 //set up initial steps left//
-var stepsLeft = 15;
+var stepsLeft = 5;
 //set up initial current wins//
-var wins = 0;
+var currentWins = 0;
 //set up initial letter guessed for each round//
 var letterGuessed = [];
 //set up games remaining//
-var gameRemaining = 3;
+var gameRemaining = 4;
 //set up initial animal on guess (non-visible//
-var animal = "bear";
+var animalArray = ["bear", "elephant", "dolphin", "penguin"];
+//index of animal to be guessed//
+var animalIndex = 0;
+
+//set up initial animal on guess (invisible)//
+var resultArray = [];
 //set up initial animal on guess (visible)//
-var result = "____";
+var result = resultArray.join(" ");
+//function for changing game status//
+
+function game() {
+  //set up animal to be guessed//
+  var animalToBeGuessed = animalArray[animalIndex];
+  for (var i = 0; i < animalToBeGuessed.length; i++) {
+    resultArray.push("_");
+  }
+
+  //function for showing all the game stats//
+  resultTextContent = document.getElementById("resultText");
+  resultContent = document.getElementById("showResult");
+  stepsLeftContent = document.getElementById("stepsLeft");
+  currentWinsContent = document.getElementById("currentWins");
+  letterGuessedContent = document.getElementById("lettersGuessed");
+  gamesRemainingContent = document.getElementById("gamesRemaining");
+  //link source of html content to actual html elements//
+  resultTextContent.innerHTML = resultText;
+  resultContent.innerHTML = resultArray.join(" ");
+  stepsLeftContent.innerHTML = stepsLeft;
+  currentWinsContent.innerHTML = currentWins;
+  letterGuessedContent.innerHTML = letterGuessed;
+  gamesRemainingContent.innerHTML = gameRemaining;
+}
+
+function getNextGame() {
+  stepsLeft = 5;
+  letterGuessed = [];
+  animalIndex++;
+}
+
+function endOfGame() {
+  isGameOn: false;
+}
+//function for changing game banner//
+function bannerChange() {
+  document.getElementById("bannerText").innerHTML = "GAME STARTED! JUST ENJOY!";
+}
 
 //press a button to change the status of the game to start//
-//when game starts change the banner//
+document.onkeyup = function() {
+  var userInput = event.key;
+
+  if (!isGameOn) {
+    game();
+    //when game starts change the banner//
+    bannerChange();
+    animalIndex = 0;
+  } else if (isGameOn == "true" && animal == "bear") {
+  }
+};
 
 //set up initial figure for game stats including: guess left, game left, array for guessed words, current wins.//
 
